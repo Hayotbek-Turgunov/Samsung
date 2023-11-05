138e39fb93a173582d248067b55183022eedf8a3
Hayotbek-Turgunov <turgunovhayotbek12@gmail.com>
2023-11-05 20:27:06 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const search = document.querySelector('.site-header-auth-search');
+const modal = document.querySelector('.modal');
+const rest  = document.querySelector('.modal-btn');
+
+
+search.addEventListener('click', ()=>{
+     modal.classList.add('modal-show')
+})
+
+rest.addEventListener('click', ()=>{
+    modal.classList.remove('modal-show')
+})
+
+// function searchiy(){
+//     modal.classList.add('modal-show');
+// }
+
+
+// function  restiy(){
+//     modal.classList.remove('modal-show');
+// }
